e4b8fc3af3b035c8d27ba7520ec9d50c6eecaf56
Tribruin <39201800+Tribruin@users.noreply.github.com>
2019-12-10 05:57:55 -0600

    Day 9 Complete

--- Intcode.py
+++ Intcode.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python3
+import sys
 
 class IntcodeComputer:
 
-	import sys
 # 	from itertools import permutations
 	
 	def __init__ (self, program, logLevel):
@@ -17,24 +17,61 @@ class IntcodeComputer:
 		self.manualInput = True
 		self.manualOutput = True
 		self.logLevel = logLevel
-		self.validInstructions = [1,2,3,4,5,6,7,8,99]
+		self.validInstructions = [1,2,3,4,5,6,7,8,9,99]
 		
 	def __printToLog(self, level, out):
 		if level <= self.logLevel :
 			print(out, file = sys.stderr)
 		return
 		
-	def updateLocation(self, location, value):
+	def writeLocation(self, locValue, value):
+		checkValue, mask = locValue
+		if mask == '0':
+			location = checkValue
+		elif mask == '2':
+			location = self.relativeBase + checkValue
+		else:
+			self.__printToLog(0, "*** ILLEGAL MASK {0} ***".format(mask))
+			
+		self.__printToLog(3,"\t\t\tStoring {0} in Location: {1}".format(value, location))
+		if location > len(self.programCode) - 1:
+			self.__printToLog(3, "\t\t\tExtending Program Code by: {0} steps".format(location - len(self.programCode) + 1))
+			self.__printToLog(3, "\t\t\tNew Program Code Lenght: {0}".format(len(self.programCode)))
+			self.programCode.extend((location - len(self.programCode) + 1) * ['0'])
 		self.programCode[location] = str(value)
-		return
+		return location
 		
-	def readFromFile(self, inputFileName):
-		self.manualInput = False
-		self.inFile = open(inputFileName, 'r')
-
+	def readFromLocation(self, locValue):
+		checkValue, mask = locValue
+		if mask == '0':
+			location = checkValue
+			x = self.programCode[location]
+			self.__printToLog(2, "\t\tGetting Position Value {0} from Position {1}".format(x, location))
+		elif mask == '1':
+			self.__printToLog(2, "\t\tGetting Immediate Value {0} ".format(checkValue))
+			return checkValue, "N/A"
+		elif mask == '2':
+			location = self.relativeBase + checkValue
+			x = self.programCode[location]
+			self.__printToLog(2, "\t\tGetting Relative Value {0} from Position {1} using Relative Base: {2} and Offset: {3}".format(x,location, self.relativeBase, location ))
+		else:
+			self.__printToLog(0, "*** ILLEGAL MASK {0} ***".format(mask))		
+	
+		# Check to see if location is outside current program, if so extend
+		self.__printToLog(3,"\t\t\tReading from Location: {0}".format(location))
+		if location > len(self.programCode) + 1:
+			self.__printToLog(3, "\t\t\tExtending Program Code by: {0} steps".format(location - len(self.programCode) + 1))
+			self.programCode.extend((location - len(self.programCode) + 1) * ['0'])
+		returnValue = self.programCode[location]
+		self.__printToLog(3,"\t\t\tReturn Value: {0}".format(location))
+		return x, location
+			
+# 	def readFromFile(self, inputFileName):
+# 		self.manualInput = False
+# 		self.inFile = open(inputFileName, 'r')
 
 	def getParameters(self, number, mask):
-		""" Get the parameters based on 'number' """
+		""" Get the parameters based on 'number' of parameters"""
 		strParameters = self.programCode[self.currentIndex+1 : self.currentIndex + number + 1]
 		parameters = list(map(lambda x : int(x), strParameters))
 		parms = self.evaluateParms(parameters, mask)
@@ -43,25 +80,19 @@ class IntcodeComputer:
 		
 	def evaluateParms(self, parms, mask):
 		""" Evaluate Parms base on Mask and return real Parameter Values """
-		
 		self.__printToLog(1, "\t\tPassed Parameters: {0}".format(parms))
+		lenOfParms = len(parms)
 		returnParms=[]
-		for i in range(len(parms)-1):
-			maskCode = mask[0]
+		for i in range(lenOfParms-1):
+			maskCode = mask[i]
+			#Changed mask from 0 to i
 			parmValue = parms[i]
-			if maskCode == '0':
-				self.__printToLog(2, "\t\tGetting Position Value {0} from Position {1}".format(self.programCode[parmValue], parmValue))
-				returnParms.append(int(self.programCode[parmValue]))
-			elif maskCode == '1':
-				self.__printToLog(2, "\t\tGetting Immediate Value {0} ".format(parmValue))
-				returnParms.append(parmValue)
-			else:
-				self.__printToLog(0, "*** ILLEGAL MASK {0} ***".format(mask))
-				exit()
-			mask = mask[1:]
+			outValue = (parmValue, maskCode)
+			value, location = self.readFromLocation(outValue)
+			returnParms.append(int(value))
 					
 # 		Append Location store
-		maskCode = mask[0]
+		maskCode = mask[lenOfParms-1]
 		returnParms.append(int(parms[-1]))
  			
 		return returnParms
@@ -70,13 +101,13 @@ class IntcodeComputer:
 	def getInstruction(self):
 		""" Get the next instruction code"""
 		nextInstruction = self.programCode[self.currentIndex].zfill(5)
-		self.__printToLog(1,"Received Instruction Code {1} from Position {0}".format(self.currentIndex, nextInstruction))
+		self.__printToLog(1,"Step: {2} - Received Instruction Code {1} from Position {0}".format(self.currentIndex, nextInstruction, self.steps))
 		mask = nextInstruction[0:3][::-1]
 		instruction = int(nextInstruction[3:5])
 		return instruction, mask
 		
-	def returnCode(self, location):
-		return int(self.programCode[location])
+# 	def returnCode(self, location):
+# 		return int(self.programCode[location])
 		
 	def execCode(self):
 		""" Execute Code """
@@ -85,7 +116,7 @@ class IntcodeComputer:
 		
 			self.steps += 1
 			self.__printToLog(1,"--------------------------------------------------")
-			self.__printToLog(3, self.programCode)
+			self.__printToLog(4, self.programCode)
 			instruction, mask = self.getInstruction()		
 			self.__printToLog(2, "\tExecuting Code: {0} with Mask: {1}".format(instruction, mask))
 	
@@ -95,48 +126,46 @@ class IntcodeComputer:
 		
 			if instruction == 1:
 				parms = self.getParameters(3, mask)
-				x, y, outputLocation = parms
+				x, y, outputLoc = parms
 				a = x + y
-				self.updateLocation(outputLocation, a)
+				outputArray = (outputLoc, mask[2])
+				outputLocation = self.writeLocation(outputArray, a)
 				self.__printToLog(2, "\tExecute {0} + {1} with result: {2} and stored in Position: {3}".format(x,y, a, outputLocation))
 				self.currentIndex += 4
 
 			
 			elif instruction == 2:
 				parms = self.getParameters(3, mask)
-				x, y, outputLocation = parms
+				x, y, outputLoc = parms
 				a = x * y
-				self.updateLocation(outputLocation, a)
+				outputArray = (outputLoc, mask[2])
+				outputLocation = self.writeLocation(outputArray, a)
 				self.__printToLog(2, "\tExecute {0} * {1} with result: {2} and stored in Position: {3}".format(x,y, a, outputLocation))
 				self.currentIndex += 4
 				
 			elif instruction == 3:
 				parms = self.getParameters(1, mask)
-				outputLocation = parms[-1]
+				outputLoc = (int(parms[0]), mask[0])
 				x = input()
-				self.updateLocation(outputLocation,x)
+				outputLocation = self.writeLocation(outputLoc,x)
+				
 				self.__printToLog(2, "\tReceived Input {0} and stored in Position: {1}".format(x,outputLocation))
 				self.currentIndex += 2
 			
 			elif instruction == 4:
 				parms = self.getParameters(1, mask)
-				if mask[0] == '0':
-					inputLocation = parms[0]
-					x = self.returnCode(inputLocation)
-				else:
-					x = int(parms[0])
-					inputLocation = "N/A"
+				outputLoc = (parms[0], mask[0])
+				location = parms[0]
+				x, inputLocation = self.readFromLocation(outputLoc)
 				print(x)
 				self.__printToLog(2, "\tPrinted value: {0} from Position: {1}".format(x, inputLocation))
 				self.currentIndex += 2
 
 			elif instruction == 5:
 				parms = self.getParameters(2, mask)
-				valueToCheck = parms[0]
-				if mask[1] == '0':
-					newLocation = self.programCode[parms[1]]
-				else:
-					newLocation = parms[1]
+				valueToCheck, locValue = parms
+				outputLoc = (locValue, mask[1])
+				newLocation, inputLocation = self.readFromLocation(outputLoc)
 				if valueToCheck:
 					self.currentIndex = int(newLocation)
 				else: 
@@ -146,11 +175,9 @@ class IntcodeComputer:
 				
 			elif instruction == 6:
 				parms = self.getParameters(2, mask)
-				valueToCheck = parms[0]
-				if mask[1] == '0':
-					newLocation = self.programCode[parms[1]]
-				else:
-					newLocation = parms[1]
+				valueToCheck, locValue = parms
+				outputLoc = (locValue, mask[1])
+				newLocation, inputLocation = self.readFromLocation(outputLoc)
 				if not valueToCheck:
 					self.currentIndex = newLocation
 				else: 
@@ -160,23 +187,32 @@ class IntcodeComputer:
 
 			elif instruction == 7:
 				parms = self.getParameters(3, mask)
-				valueToCheck1, valueToCheck2, outputLocation = parms
+				valueToCheck1, valueToCheck2, locValue = parms
+				outputLoc = (locValue, mask[2])
 				result = (valueToCheck1 < valueToCheck2)
-				self.updateLocation(outputLocation, int(result))
-				self.__printToLog(2, "\tValue {0} > {1} is {2}, storing {3} in Position: {2}".format(valueToCheck1, valueToCheck2, result, int(result)))
+				location = self.writeLocation(outputLoc, int(result))
+				self.__printToLog(2, "\tValue {0} < {1} is {2}, storing {3} in Position: {4}".format(valueToCheck1, valueToCheck2, result, int(result), location))
 				self.currentIndex += 4
 			
 			elif instruction == 8:
 				parms = self.getParameters(3, mask)
-				valueToCheck1, valueToCheck2, outputLocation = parms
+				valueToCheck1, valueToCheck2, locValue = parms
+				outputLoc = (locValue, mask[2])
 				result = (valueToCheck1 == valueToCheck2)
-				self.updateLocation(outputLocation, int(result))
-				self.__printToLog(2, "\tValue {0} > {1} is {2}, storing {3} in Position: {2}".format(valueToCheck1, valueToCheck2, result, int(result)))
-				self.currentIndex += 4				
-				
+				outputLocation = self.writeLocation(outputLoc, int(result))
+				self.__printToLog(2, "\tValue {0} == {1} is {2}, storing {3} in Position: {4}".format(valueToCheck1, valueToCheck2, result, int(result), outputLocation))
+				self.currentIndex += 4	
+			
+			elif instruction == 9:
+				parms = self.getParameters(1, mask)
+				outputLoc = (parms[0], mask[0])
+				baseUpdate, location = self.readFromLocation(outputLoc)
+				self.relativeBase += int(baseUpdate)
+				self.__printToLog(2, "\tUpdating RELATIVEBASE by {0} to {1}".format(baseUpdate, self.relativeBase))
+				self.currentIndex += 2
 			
 			else:
-				self.__printToLog(1, "BREAK-BREAK-BREAK")
+				self.__printToLog(1, "**********************\n*** END OF PROGRAM ***\n**********************")
 				# Executing Code 99
 				break
 			
